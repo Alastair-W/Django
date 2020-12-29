dafff0bb297fef9e821382c569dfde21fbf7ddc4
RowdyOnline <rowdy14@gmail.com>
2020-12-28 20:38:06 -0800

    ORM exercise complete

--- django_fundamentals/orm_project/orm_app/models.py
+++ django_fundamentals/orm_project/orm_app/models.py
@@ -1,12 +1,20 @@
 from django.db import models
     
-class Movie(models.Model):
-    title = models.CharField(max_length=45)
-    description = models.TextField()
-    release_date = models.DateTimeField()
-    duration = models.IntegerField()
+# class Movie(models.Model):
+#     title = models.CharField(max_length=45)
+#     description = models.TextField()
+#     release_date = models.DateTimeField()
+#     duration = models.IntegerField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __repr__(self):
-        return f"<Movie object: {self.title} ({self.id})>"
+    # def __repr__(self):
+    #     return f"<Movie object: {self.title} ({self.id})>"
