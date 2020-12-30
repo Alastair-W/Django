c796c648e03339a0b118941a749fd1b49df624ae
RowdyOnline <rowdy14@gmail.com>
2020-12-29 21:30:18 -0800

    Form Template assignment complete

--- django_fundamentals/orm_project/orm_app/views.py
+++ django_fundamentals/orm_project/orm_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
+def index(request):
+    context = {
+        "all_users": User.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def add(request):
+    User.objects.create(
+        first_name=request.POST['firstName'], 
+        last_name=request.POST['lastName'], 
+        email_address=request.POST['email'], 
+        age=request.POST['age']
+    )
+    return redirect('/')
