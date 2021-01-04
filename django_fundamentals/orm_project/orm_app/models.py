ae81afd3f0d1fd56ab3c8d3929414ba33c407dc5
RowdyOnline <rowdy14@gmail.com>
2021-01-04 09:45:40 -0800

    new

--- django_fundamentals/orm_project/orm_app/models.py
+++ django_fundamentals/orm_project/orm_app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
     
-# class Movie(models.Model):
-#     title = models.CharField(max_length=45)
-#     description = models.TextField()
-#     release_date = models.DateTimeField()
-#     duration = models.IntegerField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateTimeField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"<Movie object: {self.title} ({self.id})>"
 
 class User(models.Model):
     first_name = models.CharField(max_length=255)
@@ -16,5 +19,5 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # def __repr__(self):
-    #     return f"<Movie object: {self.title} ({self.id})>"
+    def __repr__(self):
+        return f"<User object: {self.first_name} {self.last_name} ({self.id})>"
