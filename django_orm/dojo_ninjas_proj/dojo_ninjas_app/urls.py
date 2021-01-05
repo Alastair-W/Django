160ca3d1047803a4b2188ca85c982452825683c6
RowdyOnline <rowdy14@gmail.com>
2021-01-05 14:49:05 -0800

    All complete except for showing member count

--- django_orm/dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ django_orm/dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('dojoForm', views.dojoForm),
-    path('ninjaForm', views.ninjaForm)
+    path('ninjaForm', views.ninjaForm),
+    path('deleteDojo', views.deleteDojo)
 ]
--- django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -4,7 +4,7 @@ from .models import Dojo, Ninja
 # Create your views here.
 def index(request):
     context = {
-        "dojo_list": Dojo.objects.all(),
+        "dojo_list": Dojo.objects.all()
     }
     return render(request, 'index.html', context)
 
@@ -12,16 +12,21 @@ def dojoForm(request):
     Dojo.objects.create(
         name=request.POST['name'], 
         city=request.POST['city'], 
-        state=request.POST['state']
+        state=request.POST['state'],
     )
     return redirect('/')
-
-
 
 def ninjaForm(request):
     Ninja.objects.create(
         first_name=request.POST['fname'], 
         last_name=request.POST['lname'], 
-        dojo=request.POST['dojo']
+        dojo_id=request.POST['dojo'],
     )
+    return redirect('/')
+
+def deleteDojo(request):
+    dojo_deleted = Dojo.objects.filter(id = request.POST.get('deleteBtn'))
+    # for x in dojo_deleted.ninjas.all():
+    #     x.delete()
+    dojo_deleted.delete()
     return redirect('/')
