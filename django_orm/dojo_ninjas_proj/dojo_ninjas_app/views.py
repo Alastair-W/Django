4680564c30c0d297dccba7616cd35ec122cfd0c7
RowdyOnline <rowdy14@gmail.com>
2021-01-04 22:14:24 -0800

    Updated_ WIP on displaying members

--- django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 # Create your views here.
+def index(request):
+    context = {
+        "dojo_list": Dojo.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+def dojoForm(request):
+    Dojo.objects.create(
+        name=request.POST['name'], 
+        city=request.POST['city'], 
+        state=request.POST['state']
+    )
+    return redirect('/')
+
+
+
+def ninjaForm(request):
+    Ninja.objects.create(
+        first_name=request.POST['fname'], 
+        last_name=request.POST['lname'], 
+        dojo=request.POST['dojo']
+    )
+    return redirect('/')
